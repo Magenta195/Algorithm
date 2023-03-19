7725c57f510b9b2abe575dc5257d18304babf6fb
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2023-03-19 20:59:51 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array, int height) {
+    int answer = 0;
+    for (auto num : array) if (num > height) answer++;
+    return answer;
+}
