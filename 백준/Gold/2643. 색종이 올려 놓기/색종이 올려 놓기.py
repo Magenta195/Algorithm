077493feb4c69be78d635555588b82f2cacd3bcf
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2023-03-21 19:18:28 +0900

    [Gold IV] Title: 색종이 올려 놓기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2643. 색종이 올려 놓기/색종이 올려 놓기.py
+++ 백준/Gold/2643. 색종이 올려 놓기/색종이 올려 놓기.py
@@ -1,14 +1,8 @@
 N = int(input())
 
 def init() :
-  paper_list = list()
-  for _ in range(N) :
-    h, w = map(int, input().split())
-    if h < w :
-      h, w = w, h
-    paper_list.append((h, w))
-    
-  paper_list.sort(reverse = True)
+  paper_list = [list(map(int, input().split())) for _ in range(N)]
+  paper_list.sort(key = lambda x : -x[0]*x[1])
   return paper_list
 
 def make_dp(paper_list) :
@@ -18,7 +12,7 @@ def make_dp(paper_list) :
     ih, iw = paper_list[i]
     for j in range(i+1, N) :
       jh, jw = paper_list[j]
-      if ih >= jh and iw >= jw :
+      if ih >= jh and iw >= jw or ih >= jw and iw >= jh :
         dp[j] = max(dp[j], dp[i] + 1)
 
   print(max(dp))
