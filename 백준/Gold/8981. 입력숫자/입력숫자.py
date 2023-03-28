cad9b9aaa8fcad1ebdaf73f9e17550da3a43ec6e
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2023-03-28 20:45:37 +0900

    [Gold V] Title: 입력숫자, Time: 3656 ms, Memory: 173132 KB -BaekjoonHub

--- 백준/Gold/8981. 입력숫자/입력숫자.py
+++ 백준/Gold/8981. 입력숫자/입력숫자.py
@@ -2,18 +2,45 @@ import sys
 
 N = int(input())
 result_list = list(map(int, input().split()))
+result_list.reverse()
 
 answer_list = [0]*N
 
-answer_list[0] = result_list[0]
-idx = answer_list[0] % N
+def is_inside_zeros(start, end) :
+  if start < end :
+    return 0 in answer_list[start:end]
+  else :
+    return 0 in answer_list[:end] and 0 in answer_list[start:]
 
-for value in result_list[1:] :
-  while answer_list[idx] :
-    idx = (idx + 1) % N
+def dfs(frm, idx) :
+  answer_list[frm] = result_list[idx]
+  if idx == N-1 :
+    if frm == 0 :
+      print(N)
+      print(*answer_list)
+      return True
+    else :
+      answer_list[frm] = 0
+      return False
 
-  answer_list[idx] = value
-  idx = (idx + value) % N
+  is_enable = False
+  _frm = frm
+  while True :
+    prev_frm = ( _frm - result_list[idx+1] ) % N
+    if not answer_list[prev_frm]:
+      is_enable |= dfs(prev_frm, idx+1)
+    _frm = (_frm - 1) % N
+    if answer_list[_frm] or _frm == frm :
+      break
 
-print(N)
-print(*answer_list)
+  answer_list[frm] = 0
+  return is_enable
+
+flg = False
+for i in range(N) :
+  flg |= dfs(i, 0)
+  if flg :
+    break
+if not flg :
+  print(-1)
+    
