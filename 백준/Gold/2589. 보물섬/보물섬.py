7502c2877e05de07ab25706b95973f990a02734e
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2023-03-31 06:26:00 +0900

    [Gold V] Title: 보물섬, Time: 720 ms, Memory: 122084 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -1,48 +1,30 @@
-
 import sys
 from collections import deque
-
 input = sys.stdin.readline
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
 
-def init() :
-  global N, M
-  N, M = map(int, input().split())
-  map_list = [input().strip() for _ in range(N)]
+N, M = map(int, input().split())
+map_list = [list(map(str, input())) for _ in range(N)]
 
-  return map_list
+result = 0
+for i in range(N) :
+  for j in range(M) :
+    if map_list[i][j] == 'L' :
+      visited = [[False]*M for _ in range(N)]
 
-def bfs(x, y, map_list) :
-  global N, M
-  visited = [[False]*M for _ in range(N)]
+      q = deque()
+      q.append((j, i, 0))
+      visited[i][j] = True
 
-  q = deque([(x, y, 0)])
-  visited[y][x] = True
+      while q :
+        x, y, dist = q.popleft()
+          
+        for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1) ]:
+          ax, ay = x + dx, y + dy
+          if -1 < ax < M and -1 < ay < N and not visited[ay][ax] and map_list[ay][ax] == 'L' :
+            visited[ay][ax] = True
+            q.append((ax, ay, dist+1))
 
-  max_dist = 0
-  while q :
-    x, y, dist = q.popleft()
-    max_dist = max(dist, max_dist)
-    for k in range(4) :
-      ax, ay = x + dx[k], y + dy[k]
-      if -1 < ax < M and -1 < ay < N and map_list[ay][ax] == 'L' and not visited[ay][ax] :
-        visited[ay][ax] = True
-        q.append((ax, ay, dist+1))
+      if dist > result :
+        result = dist
 
-  return max_dist
-
-def search(map_list) :
-  result = 0
-  for i in range(N) :
-    for j in range(M) :
-      if map_list[i][j] == 'L' :
-        result = max(result, bfs(j, i, map_list))
-
-  print(result)
-
-def solve() :
-  map_list = init()
-  search(map_list)
-
-solve()
+print(result)
