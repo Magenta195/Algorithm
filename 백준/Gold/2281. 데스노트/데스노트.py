bf98f74eaa58b4a4c0dc651ab8d376bb3f357597
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2023-05-16 18:51:49 +0900

    [Gold III] Title: 데스노트, Time: 440 ms, Memory: 31396 KB -BaekjoonHub

--- 백준/Gold/2281. 데스노트/데스노트.py
+++ 백준/Gold/2281. 데스노트/데스노트.py
@@ -5,27 +5,24 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 name_list = [int(input()) for _ in range(n)]
+MAX = float('inf')
+dp = [MAX]*(n+1)
+dp[n] = 0
 
-dp = [[-1]*n for _ in range(n)]
+def dfs(idx) :
+  if dp[idx] < MAX :
+    return dp[idx]
 
-def dfs(line, idx) :
-  if idx == n :
-    return 0
-  if dp[line][idx] > -1 :
-    return dp[line][idx]
-  if sum(name_list[idx:]) + (n - idx) - 1 <= m :
-    dp[line][idx] = 0
-    return 0
-  
-  dp[line][idx] = float('inf')
-  next_idx = idx
-  left = m
-  while next_idx < n and left >= name_list[next_idx] :
-    left -= name_list[next_idx]
-    dp[line][idx] = min(dp[line][idx], left ** 2 + dfs(line+1, next_idx+1))
-    left -= 1
+  left = m - name_list[idx]
+  next_idx = idx+1
+  while next_idx <= n and left >= 0 :
+    if next_idx == n :
+      dp[idx] = 0
+      break
+    dp[idx] = min(dp[idx], left ** 2 + dfs(next_idx))
+    left -= 1 + name_list[next_idx]
     next_idx += 1
 
-  return dp[line][idx]
+  return dp[idx]
 
-print(dfs(0, 0))
+print(dfs(0))
