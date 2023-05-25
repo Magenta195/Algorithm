53e587976a3b305e718bd0a2482505189b0bb9e5
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2023-05-25 23:32:55 +0900

    [Gold III] Title: 문자열과 점수, Time: 616 ms, Memory: 223996 KB -BaekjoonHub

---
+++ 백준/Gold/2216. 문자열과 점수/문자열과 점수.py
@@ -0,0 +1,17 @@
+A, B, C  = map(int, input().split())
+X = input().strip()
+Y = input().strip()
+
+dp = [[-float('inf')]*(len(Y)+1) for _ in range(len(X)+1)]
+dp[0][0] = 0
+for i in range(len(X)+1) :
+  for j in range(len(Y)+1) :
+    if i < len(X) and j < len(Y) :
+      match_score = A if X[i] == Y[j] else C
+      dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j]+match_score)
+    if i < len(X) :
+      dp[i+1][j] = max(dp[i+1][j], dp[i][j] + B)
+    if j < len(Y) :
+      dp[i][j+1] = max(dp[i][j+1], dp[i][j] + B)
+
+print(dp[-1][-1])
