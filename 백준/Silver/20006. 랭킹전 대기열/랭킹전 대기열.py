e3891f956e2bde35fe11e15bd8bd7dcc56ed6f10
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2023-05-26 18:44:08 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
@@ -1,26 +1,20 @@
 p, m = map(int, input().split())
-
 room_list = list()
 
 for _ in range(p) :
   l, n = input().split()
   l = int(l)
-  if not room_list :
-    room_list.append([l, m == 1, [(l, n)]])
-  else :
-    flg = False
-    for i in range(len(room_list)) :
-      if not room_list[i][1] and abs(l - room_list[i][0]) <= 10 :
-        room_list[i][2].append((l, n))
-        flg = True
-        if len(room_list[i][2]) == m :
-          room_list[i][1] = True
-        break
-    if not flg :
-      room_list.append([l, m == 1, [(l, n)]])
+  flg = False
+  for i in range(len(room_list)) :
+    if len(room_list[i]) < m and abs(l - room_list[i][0][0]) <= 10 :
+      room_list[i].append((l, n))
+      flg = True
+      break
+  if not flg :
+    room_list.append([(l, n)])
 
-for _, start, _room_list in room_list :
-  print('Started!' if start else 'Waiting!')
+for _room_list in room_list :
+  print('Started!' if len(_room_list) == m else 'Waiting!')
   _room_list.sort(key = lambda x : x[-1])
   for l, n in _room_list :
     print(l, n)
