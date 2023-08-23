4d3fc2f7e899075b9570a297f781010a5d92069c
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2023-08-23 12:51:22 +0900

    [Platinum V] Title: 숨바꼭질 5, Time: 1356 ms, Memory: 62832 KB -BaekjoonHub

---
+++ 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.py
@@ -0,0 +1,26 @@
+from collections import deque
+MAX = 500001
+
+N, K = map(int, input().split())
+sum_list, answer_list = [0], [K]
+t = 1
+while sum_list[-1] + K < MAX :
+  sum_list.append(sum_list[-1] + t)
+  answer_list.append(sum_list[-1] + K)
+  t += 1
+
+q = deque([(N, 0)])
+visited = [[MAX]*MAX for _ in range(2)]
+visited[0][N] = 0
+answer = MAX
+while q :
+  now, t = q.popleft()
+  for nxt in [now*2, now-1, now+1] :
+    if -1 < nxt < MAX and visited[(t+1)%2][nxt] > t+1 and answer_list[t+1] < MAX :
+      visited[(t+1)%2][nxt] = t + 1
+      q.append((nxt, t+1))
+
+for t, now in enumerate(answer_list[:-1]) :
+  if visited[t%2][now] <= t :
+    answer = min(answer, t)
+print(answer if answer < MAX else -1)
