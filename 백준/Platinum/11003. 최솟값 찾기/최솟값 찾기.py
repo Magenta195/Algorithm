60df0b9de52e58348fe74f9bd3148506bffeef8a
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2023-09-02 17:16:51 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 5964 ms, Memory: 909364 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
@@ -1,13 +1,13 @@
-from collections import deque
+from heapq import heappush, heappop
+
 N, L = map(int, input().split())
 num_list = list(map(int, input().split()))
-dq = deque()
+dq = list()
 D_list = list()
+
 for i in range(N) :
-  while dq and dq[-1][0] > num_list[i] :
-    dq.pop()
-  while dq and dq[0][1] < i - L + 1 :
-    dq.popleft()
-  dq.append((num_list[i], i))
+  while dq and dq[0][1] < i - L + 1:
+    heappop(dq)
+  heappush(dq, (num_list[i], i))
   D_list.append(dq[0][0])
 print(*D_list)
