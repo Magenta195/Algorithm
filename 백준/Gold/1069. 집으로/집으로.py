de773993036505a54ddcf03211cd6be469f95a6d
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2023-10-24 12:17:31 +0900

    [Gold III] Title: 집으로, Time: 68 ms, Memory: 34160 KB -BaekjoonHub

---
+++ 백준/Gold/1069. 집으로/집으로.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+X, Y, D, T = map(int, input().split())
+dist = (X**2 + Y**2) ** 0.5
+
+if D/T < 1 :
+  print(dist)
+  exit()
+
+direct = dist // D * T
+answer = direct + min(dist % D, T + (D - dist % D), 2*T)
+if dist < 2*D :
+  answer = min(answer, 2*T)
+if dist >= 2*D :
+  answer = min(answer, direct + T)
+print(answer)
