e4b184231319437b14e1e0a9436ec195ba82e944
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2023-11-04 15:12:31 +0900

    [Platinum V] Title: 소트, Time: 64 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Platinum/1071. 소트/소트.py
+++ 백준/Platinum/1071. 소트/소트.py
@@ -1,36 +1,30 @@
 from collections import Counter
-MAXVAL = 1001
-
+Max = 1001
 N = int(input())
 
-num_maps = [0]*1001
+num_maps = [0]*Max
 for key, val in  Counter(map(int, input().split())).items() :
   num_maps[key] = val
 
 ans = list()
-
-for i in range(MAXVAL) :
+for i in range(Max) :
   if not num_maps[i] :
     continue
-  if i == MAXVAL-1 or i < MAXVAL-1 and not num_maps[i+1] :
+  if i == Max-1 or i < Max-1 and not num_maps[i+1] :
     ans += [i]*num_maps[i]
     continue
-  if i < MAXVAL-2 :
-    if not num_maps[i+2] :
-      best_idx = -1
-      for j in range(i+3, MAXVAL) :
-        if num_maps[j] :
-          best_idx = j
-          break
-      if best_idx != -1 :
-        ans += [i]*num_maps[i] + [best_idx]
-        num_maps[best_idx] -= 1
-      else :
-        ans += [i+1]*num_maps[i+1] + [i]*num_maps[i]
-        num_maps[i+1] = 0
+  if i < Max-2 :
+    best_idx = -1
+    for j in range(i+2, Max) :
+      if num_maps[j] :
+        best_idx = j
+        break
+    if best_idx != -1 :
+      ans += [i]*num_maps[i] + [best_idx]
+      num_maps[best_idx] -= 1
     else :
-      ans += [i]*num_maps[i] + [i+2]
-      num_maps[i+2] -= 1
+      ans += [i+1]*num_maps[i+1] + [i]*num_maps[i]
+      num_maps[i+1] = 0
   else :
     ans += [i+1]*num_maps[i+1] + [i]*num_maps[i]
     num_maps[i+1] = 0
