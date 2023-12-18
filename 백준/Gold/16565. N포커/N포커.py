07d22700e02c30562fe33d54c6fb5de62da5d246
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2023-12-18 20:21:28 +0900

    [Gold II] Title: N포커, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/16565. N포커/N포커.py
@@ -0,0 +1,23 @@
+MOD = 10007
+N = int(input())
+if N < 4 :
+  print(0)
+  exit()
+if N == 52 :
+  print(1)
+  exit()
+comb = [[0]*53 for _ in range(53)]
+comb[1][0] = comb[1][1] = 1
+for i in range(2, 53) :
+  comb[i][0] = comb[i][i] = 1
+  for j in range(1, i) :
+    comb[i][j] = comb[i-1][j] + comb[i-1][j-1]
+result = 0
+cnt, pos = 1, 1
+while N >= 4 :
+  result += pos * comb[13][cnt] * comb[52-cnt*4][N-4]
+  cnt += 1
+  pos = -pos
+  N -= 4
+
+print(result % MOD)
