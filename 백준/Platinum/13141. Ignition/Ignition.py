37bc5692ffd0a69a97685f04e81d422b14f416f2
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2023-12-18 11:02:36 +0900

    [Platinum V] Title: Ignition, Time: 852 ms, Memory: 115636 KB -BaekjoonHub

--- 백준/Platinum/13141. Ignition/Ignition.py
+++ 백준/Platinum/13141. Ignition/Ignition.py
@@ -30,7 +30,7 @@ def bfs(init) :
 
   result = 0.
   for i in range(1, N+1) :
-    for j in range(1, N+1) :
+    for j in range(i, N+1) :
       if not max_edge[i][j] :
         continue
       result = max(result, visited[i], visited[j])
