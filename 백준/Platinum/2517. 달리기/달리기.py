371afa3b6022527584dff69113c4f0b038aafa5e
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2024-01-04 11:36:40 +0900

    [Platinum IV] Title: 달리기, Time: 2356 ms, Memory: 185344 KB -BaekjoonHub

--- 백준/Platinum/2517. 달리기/달리기.py
+++ 백준/Platinum/2517. 달리기/달리기.py
@@ -1,48 +1,43 @@
-from collections import deque
+from bisect import bisect_left
 import sys
 input = sys.stdin.readline
-MAX = float('inf')
 
 N = int(input())
 runners = [int(input()) for _ in range(N)]
+rank = sorted(runners)
+runners = [bisect_left(rank, r) for r in runners]
 
-class MergeSortTree :
+class SegTree :
   def __init__(self) :
-    self.tree = [list() for _ in range(4*N)]
-    def _init(start, end, idx) :
+    self.tree = [0]*(4*N)
+    
+  def update(self, target) :
+    def _update(start, end, idx) :
       if start == end :
-        self.tree[idx] = [runners[start]]
+        self.tree[idx] += 1
         return
       mid = (start + end) // 2
-      _init(start, mid, idx*2)
-      _init(mid+1, end, idx*2+1)
+      if target <= mid :
+        _update(start, mid, idx*2)
+      else :
+        _update(mid+1, end, idx*2+1)
       self.tree[idx] = self.tree[idx*2] + self.tree[idx*2+1]
-      self.tree[idx].sort()
-    _init(0, N-1, 1)
-
-  def search(self, right) :
-    val = runners[right]
-    right -= 1
-    def upper_bound(idx) :
-      start, end = 0, len(self.tree[idx])
-      while start < end :
-        mid = (start + end) // 2
-        if self.tree[idx][mid] <= val :
-          start = mid + 1
-        else :
-          end = mid
-      return len(self.tree[idx]) - end
-    def _search(start, end, idx) :
-      if right < start :
-        return 0
-      if start <= end <= right :
-        return upper_bound(idx)
+    _update(0, N-1, 1)
+  
+  def search(self, target, order) :
+    start, end, idx, val = 0, N-1, 1, 0
+    while start < end :
       mid = (start + end) // 2
-      lval = _search(start, mid, idx*2)
-      rval = _search(mid+1, end, idx*2+1)
-      return lval + rval
-    print(_search(0, N-1, 1) + 1)
+      if target >= mid :
+        start = mid + 1
+        val += self.tree[idx*2]
+        idx = idx*2+1
+      else :
+        end = mid
+        idx = idx*2
+    print(order + 1 - val)
 
-tree = MergeSortTree()
+tree = SegTree()
 for i in range(N) :
-  tree.search(i)
+  tree.search(runners[i], i)
+  tree.update(runners[i])
