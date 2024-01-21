c9bb9dc0a8a1a9e4bde7ec714b8c1637d1e565e9
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2024-01-21 15:30:20 +0900

    [Platinum IV] Title: 석양이 진다..., Time: 1056 ms, Memory: 55380 KB -BaekjoonHub

--- 백준/Platinum/13352. 석양이 진다．．．/석양이 진다．．．.py
+++ 백준/Platinum/13352. 석양이 진다．．．/석양이 진다．．．.py
@@ -3,28 +3,27 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-coord_list = [list(map(int, input().split())) + [i] for i in range(N)]
-
+coord_list = [tuple(map(int, input().split())) for _ in range(N)]
+if N < 4 :
+  print("success")
+  exit()
 def ccw(a, b, c) :
   return (a[0]*b[1] + b[0]*c[1] + c[0]*a[1]) - (a[1]*b[0] + b[1]*c[0] + c[1]*a[0])
 
-def search(coord) :
+def search(coord, ret) :
   next_coord = []
-  ret = coord[:2]
-  for x, y, i in coord[2:] :
-    if ccw(ret[-2], ret[-1], (x, y)) != 0 :
-      next_coord.append([x, y, i])
+  for _coord in coord :
+    if ccw(ret[-2], ret[-1], _coord) != 0 :
+      next_coord.append(_coord)
   return next_coord
 
 flg = False
 for _ in range(10) :
-  idx = list(range(N))
-  random.shuffle(idx)
-  tmp = sorted(coord_list, key = lambda x : idx[x[-1]])
-  
-  for _ in range(2) :
-    tmp = search(tmp)
-    if not tmp :
+  tmp = coord_list[:]
+  for i in range(2) :
+    ret = random.sample(tmp, 2)
+    tmp = search(tmp, ret)
+    if len(tmp) < 2-i :
       flg = True
       break
   if flg :
