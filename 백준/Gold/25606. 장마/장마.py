311d80e278284e90334c2d9a612ef4d913fefa00
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2024-02-07 13:20:06 +0900

    [Gold I] Title: 장마, Time: 264 ms, Memory: 135440 KB -BaekjoonHub

--- 백준/Gold/25606. 장마/장마.py
+++ 백준/Gold/25606. 장마/장마.py
@@ -7,20 +7,19 @@ rain = list(map(int, input().split()))
 
 sums = [0]*N
 evap = [0]*N
-q = defaultdict(lambda : [0, 0])
+q = [[0]*2 for _ in range(N)]
 cnt = 0
 
 for i in range(N) :
   sums[i] = rain[i]
   if i > 0 :
     sums[i] += sums[i-1]
-  if i in q :
-    evap[i] += q[i][1]
-    cnt -= q[i][0]
-    del q[i]
-  evap[i] += cnt * M
-  q[i+1+rain[i]//M][0] += 1
-  q[i+1+rain[i]//M][1] += rain[i] % M
+  cnt -= q[i][0]
+  evap[i] += q[i][1] + cnt * M
+  idx = i+1+rain[i] // M
+  if idx < N :
+    q[i+1+rain[i]//M][0] += 1
+    q[i+1+rain[i]//M][1] += rain[i] % M
   cnt += 1
   
 for i in range(1, N) :
