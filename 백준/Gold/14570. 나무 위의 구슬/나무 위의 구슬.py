869718a67ac74982782171c2bce091112761d183
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2024-02-13 22:27:42 +0900

    [Gold I] Title: 나무 위의 구슬, Time: 304 ms, Memory: 66012 KB -BaekjoonHub

--- 백준/Gold/14570. 나무 위의 구슬/나무 위의 구슬.py
+++ 백준/Gold/14570. 나무 위의 구슬/나무 위의 구슬.py
@@ -1,21 +1,18 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(300000)
 
 N = int(input())
 children = [[0, 0]] + [list(map(int, input().split())) for _ in range(N)]
-K = int(input())
+K = int(input()) - 1
 
-def dfs(node, k) :
+node = 1
+while True :
   if children[node][0] == children[node][1] == -1 :
-    return node
+    print(node)
+    break
   if children[node][0] == -1 :
-    return dfs(children[node][1], k)
-  if children[node][1] == -1 :
-    return dfs(children[node][0], k)
-
-  if k % 2 == 0 :
-    return dfs(children[node][0], k // 2)
-  return dfs(children[node][1], k // 2)
-
-print(dfs(1, K-1))
+    node = children[node][1]
+  elif children[node][1] == -1 :
+    node = children[node][0]
+  else :
+    node, K = children[node][K % 2], K // 2
