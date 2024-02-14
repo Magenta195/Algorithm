7c8e21c5237d80601df497bd1f86f2bd7ecf231b
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2024-02-14 17:53:29 +0900

    [Gold II] Title: 카드 팩 구매하기, Time: 560 ms, Memory: 51240 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Gold II/15823. 카드 팩 구매하기/카드 팩 구매하기.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+N, M = map(int, input().split())
+cards = list(map(int, input().split()))
+
+q = deque()
+visited = set()
+max_len = [0]*N
+for i, c in enumerate(cards) :
+  q.append((c, i))
+  if c not in visited :
+    visited.add(c)
+    continue
+  while q :
+    _c, j = q.popleft()
+    max_len[j] = i - j
+    if _c == c :
+      break
+    visited.discard(_c)
+while q :
+  _c, j = q.popleft()
+  max_len[j] = N - j
+  
+def calculate(length) :
+  dp = [0]*(N+1)
+  for i in range(N) :
+    if max_len[i] >= length :
+      dp[i+length] = max(dp[i+length], dp[i] + 1)
+    dp[i+1] = max(dp[i], dp[i+1])
+  return max(dp) >= M
+  
+def bisect() :
+  start, end = 0, N // M + 1
+  ret = 0
+  while start < end :
+    mid = (start + end) // 2
+    if calculate(mid) :
+      ret = mid * M
+      start = mid + 1
+    else :
+      end = mid
+  print(ret)
+
+bisect()
