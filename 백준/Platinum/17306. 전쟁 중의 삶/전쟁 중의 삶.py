b400d528c1706443fdb6ca6cab6f01d255577642
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2024-02-20 16:57:08 +0900

    [Platinum III] Title: 전쟁 중의 삶, Time: 4592 ms, Memory: 593484 KB -BaekjoonHub

--- 백준/Platinum/17306. 전쟁 중의 삶/전쟁 중의 삶.py
+++ 백준/Platinum/17306. 전쟁 중의 삶/전쟁 중의 삶.py
@@ -1,26 +1,25 @@
 N = int(input())
 nums = list(map(int, input().split()))
+visit = set()
 
 def find_lca(a, b) :
-  ad = len(bin(a)) - 2
-  bd = len(bin(b)) - 2
+  ad = len(bin(a))-2
+  bd = len(bin(b))-2
+  visit.add(a)
+  visit.add(b)
   if ad > bd :
-    a, ad, b, bd = b, bd, a, ad
-  for _ in range(bd - ad) :
+    a, b = b, a
+  for _ in range(abs(bd - ad)) :
     b >>= 1
+    visit.add(b)
   while a != b :
-    a >>= 1
-    b >>= 1
+    a, b = a >> 1, b >> 1
+    visit.add(a)
+    visit.add(b)
   return a
 
 root = find_lca(nums[0], nums[1])
 for i in range(2, N) :
   root = find_lca(root, nums[i])
-
-visit = {root}
-for n in nums :
-  while n != root :
-    visit.add(n)
-    n >>= 1
 
 print(len(visit))
