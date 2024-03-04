cad3d23d9d479bcbc3e97d59ac5460285533d201
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2024-03-04 14:02:12 +0900

    [Gold I] Title: 최적의 능력 구성, Time: 888 ms, Memory: 126796 KB -BaekjoonHub

--- 백준/Gold/11394. 최적의 능력 구성/최적의 능력 구성.py
+++ 백준/Gold/11394. 최적의 능력 구성/최적의 능력 구성.py
@@ -1,17 +1,13 @@
-import sys
-input = sys.stdin.readline
-
 N = int(input())
-dp = [0]*(1 << N)
-skills = [list(map(int, input().split())) for _ in range(N)]
+dp = [0]*(1<<N)
+skills = [[*map(int, input().split())] for _ in range(N)]
 
 for i in range(N) :
-  dp[1 << i] = skills[i][0] / 100 * skills[i][1]
-
-for i in range(1, 1 << N) :
+  dp[1<<i] = skills[i][0]/100*skills[i][1]
+for i in range(1, 1<<N) :
   dp[i] /= bin(i).count('1')
   for j in range(N) :
     if i & (1 << j) :
       continue
-    dp[i | (1 << j)] += dp[1 << j] + (100 - skills[j][0]) / 100 * dp[i]
+    dp[i|(1<<j)] += dp[1<<j] + (100-skills[j][0])/100*dp[i]
 print(max(dp))
